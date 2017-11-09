e14c8fe84540953362039e7ba2aeb431d49b281f
Alexis <biveralexis@gmail.com>
2017-11-09 08:21:44 +0100

    object constructor and unicode

---
+++ src/objects/emojis/Emoji.java
@@ -0,0 +1,17 @@
+public class Emoji{
+
+    private String unicode;
+
+    public Emoji (String unicode){
+        this.unicode = unicode;
+    }
+
+    public String getUnicode(){
+        return this.unicode;
+    }
+
+    public void setUnicode(){
+        this.unicode = unicode;
+    }
+
+}
