0f635990c3aa6eb0f50259dcc24345475383d50a
Alexis <biveralexis@gmail.com>
2017-11-09 10:55:44 +0100

    name and emote added

--- src/objects/emojis/Emoji.java
+++ src/objects/emojis/Emoji.java
@@ -1,11 +1,18 @@
 public class Emoji{
 
     private String unicode;
+    private String name;
+    private String emote;
 
+//Constructor
     public Emoji (String unicode){
         this.unicode = unicode;
+        this.name = name;
+        this.emote = emote;
+        
     }
 
+//Setters and Getters
     public String getUnicode(){
         return this.unicode;
     }
@@ -14,4 +21,19 @@ public class Emoji{
         this.unicode = unicode;
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public String getEmote(){
+        return this.emote;
+    }
+
+    public void setEmote(){
+        this.emote = emote;
+    }
 }
